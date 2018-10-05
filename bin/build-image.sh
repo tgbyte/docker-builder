e4d945f251525f2297a1d962409d3cc47dd71a24
Thilo-Alexander Ginkel <tg@tgbyte.de>
2018-10-05 17:40:15 +0200

    (Non-)Multiarch improvements

--- bin/build-image.sh
+++ bin/build-image.sh
@@ -4,9 +4,20 @@ set -e
 
 source $(dirname $0)/../share/build-functions.sh
 
-docker build --no-cache --pull --platform ${ARCH} -t "$FULL_IMAGE_ARCH" -f "$DOCKERFILE" "$BUILD_DIR"
+if [ -z "$MULTIARCH" ]
+then
+  IMAGE_NAME=${FULL_IMAGE}
+else
+  IMAGE_NAME=${FULL_IMAGE_ARCH}
+fi
+
+docker build --no-cache --pull --platform ${ARCH} -t "$IMAGE_NAME" -f "$DOCKERFILE" "$BUILD_DIR"
 
 mkdir -p results
-docker push "$FULL_IMAGE_ARCH"
-FULL_IMAGE_ARCH_SHA=$(docker inspect --format='{{ index .RepoDigests 0 }}' "$FULL_IMAGE_ARCH")
-echo "$FULL_IMAGE_ARCH_SHA" > results/"$ARCH"
+docker push "$IMAGE_NAME"
+
+if [ -n "$MULTIARCH" ]
+then
+  FULL_IMAGE_ARCH_SHA=$(docker inspect --format='{{ index .RepoDigests 0 }}' "$IMAGE_NAME")
+  echo "$FULL_IMAGE_ARCH_SHA" > results/"$ARCH"
+fi

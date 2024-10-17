57e39468b234970a6cff9f7737663462067a45b1
Thilo-Alexander Ginkel <tg@tgbyte.de>
2024-10-17 16:41:13 +0200

    Use AWS for Trivy DB

--- bin/trivy.sh
+++ bin/trivy.sh
@@ -41,6 +41,7 @@ if [ -n "${TRIVY_REPORT_JSON}" ]; then
     --cache-dir .trivy \
     image \
     --severity "${TRIVY_SEVERITY:-HIGH,CRITICAL,MEDIUM}" \
+    --db-repository public.ecr.aws/aquasecurity/trivy-db:2 \
     "${TRIVY_PARAMS[@]}" \
     --ignore-unfixed \
     --no-progress \

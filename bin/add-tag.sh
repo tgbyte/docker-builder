607fd74c34db7a78e11127267eabb92a0a87d0c9
Thilo-Alexander Ginkel <tg@tgbyte.de>
2026-07-01 20:59:18 +0000

    add-tag: use skopeo copy --all instead of docker tag/push

--- bin/add-tag.sh
+++ bin/add-tag.sh
@@ -5,10 +5,10 @@ set -e
 # shellcheck disable=SC1091
 source "$(dirname "$0")/../share/build-functions.sh"
 
-docker_login
+registry_auth
 
 for NEW_TAG in "$@"; do
   NEW_FULL_IMAGE="${IMAGE}:${NEW_TAG}"
-  docker tag "${FULL_IMAGE}" "${NEW_FULL_IMAGE}"
-  docker push "${NEW_FULL_IMAGE}"
+  echo "Copying ${FULL_IMAGE} -> ${NEW_FULL_IMAGE}..."
+  skopeo copy --all "docker://${FULL_IMAGE}" "docker://${NEW_FULL_IMAGE}"
 done
